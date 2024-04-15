3b28fe4488eecbb36a66c262978b42d9381759b1
John <johnsief12@gmail.com>
2024-04-15 18:02:57 -0400

    set up add to cart button value

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const input = document.querySelector("#input-field");
+const btn = document.querySelector("#add-button");
+
+
+function groceryItem(){
+    let inputValue = input.value;
+    console.log(inputValue);
+}
+
+
+
+
+btn.addEventListener('click', groceryItem)
+
+
+
