79cfb305db28ecda52b86480726472b8167811f9
John <johnsief12@gmail.com>
2024-04-16 18:07:26 -0400

    added firebase to the app

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-import { add } from "./asserts/functions.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 const input = document.querySelector("#input-field");
 const btn = document.querySelector("#add-button");
 const appSettings = {
-  databaseURL: 'https://playground-d53b3-default-rtdb.firebaseio.com/',
+  databaseURL: "https://mobile-app-b351e-default-rtdb.firebaseio.com/"
 };
 
-console.log(add(2,2))
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList")
 
 function groceryItem(){
     let inputValue = input.value;
     console.log(inputValue);
+    push(shoppingListInDB, inputValue)
 }
 
 
 
 
-btn.addEventListener('click', groceryItem)
+btn.addEventListener('click', groceryItem);
 
 
 
