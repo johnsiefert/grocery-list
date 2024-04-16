3450f9bdb50b998e80344da1717508624938c818
John <johnsief12@gmail.com>
2024-04-16 17:29:03 -0400

    oops

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+import { add } from "./asserts/functions.js"
 const input = document.querySelector("#input-field");
 const btn = document.querySelector("#add-button");
+const appSettings = {
+  databaseURL: 'https://playground-d53b3-default-rtdb.firebaseio.com/',
+};
 
+console.log(add(2,2))
 
 function groceryItem(){
     let inputValue = input.value;
